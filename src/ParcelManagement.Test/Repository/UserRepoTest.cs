a3386cdf5a194abaae4e64c0a1b7ce28698cc9ad
qawitherev <qawitherev@gmail.com>
2025-08-13 14:34:35 +0800

    Added UserRepository.GetUserById() and UserRepository.GetOneUserBySpecification() unit test

--- src/ParcelManagement.Test/Repository/UserRepoTest.cs
+++ src/ParcelManagement.Test/Repository/UserRepoTest.cs
@@ -1,6 +1,9 @@
+using System.Net.Http.Headers;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
 using ParcelManagement.Core.Entities;
 using ParcelManagement.Core.Repositories;
+using ParcelManagement.Core.Specifications;
 using ParcelManagement.Infrastructure.Database;
 using ParcelManagement.Infrastructure.Repository;
 using Xunit;
@@ -59,6 +62,76 @@ namespace ParcelManagement.Test.Repository
                 var insertedUser = await dbContext.Users.FindAsync(theId);
                 Assert.NotNull(insertedUser);
                 Assert.Equal(theId, validUser.Id);
+            }
+        }
+
+        [Fact]
+        public async Task GetUserbyIdAsync_NonNull_ShouldReturnUser()
+        {
+            var theId = Guid.NewGuid();
+            var newUser = new User
+            {
+                Id = theId,
+                Username = "username",
+                Email = "this@email.com",
+                ResidentUnit = "RU001",
+                PasswordHash = "####",
+                PasswordSalt = "salt",
+            };
+
+            var dbContextOption = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "testDB").Options;
+
+            using (var dbContext = new ApplicationDbContext(dbContextOption))
+            {
+                var userRepo = new UserRepository(dbContext);
+                await dbContext.Users.AddAsync(newUser);
+                var result = await userRepo.GetUserByIdAsync(theId);
+                Assert.NotNull(result);
+                Assert.Equal(theId, result.Id);
+            }
+        }
+
+        [Fact]
+        public async Task GetUserByIdAsync_Null_ShouldNotReturnUser()
+        {
+            var dbContextOption = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+
+            using (var dbContext = new ApplicationDbContext(dbContextOption))
+            {
+                var userRepo = new UserRepository(dbContext);
+                var result = await userRepo.GetUserByIdAsync(Guid.NewGuid());
+                Assert.Null(result);
+            }
+        }
+
+        // this test is for username specification
+        [Fact]
+        public async Task GetOneUserBySpecification_NotNull_ShouldReturnUser()
+        {
+            var theUsername = "username hihi";
+            var newUser = new User
+            {
+                Id = Guid.NewGuid(),
+                Username = theUsername,
+                Email = "this@email.com",
+                ResidentUnit = "RU001",
+                PasswordHash = "####",
+                PasswordSalt = "salt",
+            };
+
+            var dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+
+            using (var dbContext = new ApplicationDbContext(dbContextOptions))
+            {
+                await dbContext.Users.AddAsync(newUser);
+                var userRepo = new UserRepository(dbContext);
+                var userByUsernameSpecification = new UserByUsernameSpecification(newUser.Username);
+                var result = await userRepo.GetOneUserBySpecification(userByUsernameSpecification);
+                Assert.NotNull(result);
+                Assert.Equal(theUsername, result.Username);                
             }
         }
     }

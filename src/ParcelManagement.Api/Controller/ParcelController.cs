6b13e4788973f5e47b336060a10410ec875a501e
Abdul Qawi Kamran <qawitherev@gmail.com>
2025-08-16 08:46:04 +0800

    Put parcelController inside a namespace

--- src/ParcelManagement.Api/Controller/ParcelController.cs
+++ src/ParcelManagement.Api/Controller/ParcelController.cs
@@ -4,76 +4,79 @@ using ParcelManagement.Api.DTO;
 using ParcelManagement.Core.Entities;
 using ParcelManagement.Core.Services;
 
-[ApiController]
-[Route("api/[controller]")]
-public class ParcelController(IParcelService parcelService) : ControllerBase
+namespace ParcelManagement.Api.Controller
 {
-    // since we wont expose this endpoint publicly, we won't follow 
-    // url pattern to give way to 
-    // {"{trackingNumber}"}
-    [HttpGet("GetParcelById/{id}")]
-    public async Task<IActionResult> GetParcelById(Guid id)
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ParcelController(IParcelService parcelService) : ControllerBase
     {
-        var parcel = await parcelService.GetParcelByIdAsync(id);
-        return Ok(parcel);
-    }
-
-    [HttpPost("checkIn")]
-    public async Task<IActionResult> CheckInParcel([FromBody] CheckInParcelDto dto)
-    {
-        if (!ModelState.IsValid)
+        // since we wont expose this endpoint publicly, we won't follow 
+        // url pattern to give way to 
+        // {"{trackingNumber}"}
+        [HttpGet("GetParcelById/{id}")]
+        public async Task<IActionResult> GetParcelById(Guid id)
         {
-            return BadRequest(ModelState);
+            var parcel = await parcelService.GetParcelByIdAsync(id);
+            return Ok(parcel);
         }
 
-        var newParcel = await parcelService.CheckInParcelAsync(dto.TrackingNumber, dto.ResidentUnit, dto.Weight, dto.Dimensions);
-        var newParcelDto = new ParcelResponseDto
+        [HttpPost("checkIn")]
+        public async Task<IActionResult> CheckInParcel([FromBody] CheckInParcelDto dto)
         {
-            Id = newParcel.Id,
-            TrackingNumber = newParcel.TrackingNumber,
-            ResidentUnit = newParcel.ResidentUnit,
-            Weight = newParcel.Weight ?? 0,
-            Dimensions = newParcel.Dimensions ?? ""
-        };
-        return CreatedAtAction(nameof(GetParcelById), new { id = newParcelDto.Id }, newParcelDto);
-    }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var newParcel = await parcelService.CheckInParcelAsync(dto.TrackingNumber, dto.ResidentUnit, dto.Weight, dto.Dimensions);
+            var newParcelDto = new ParcelResponseDto
+            {
+                Id = newParcel.Id,
+                TrackingNumber = newParcel.TrackingNumber,
+                ResidentUnit = newParcel.ResidentUnit,
+                Weight = newParcel.Weight ?? 0,
+                Dimensions = newParcel.Dimensions ?? ""
+            };
+            return CreatedAtAction(nameof(GetParcelById), new { id = newParcelDto.Id }, newParcelDto);
+        }
 
 
-    [HttpPost("{trackingNumber}/claim")]
-    public async Task<IActionResult> ClaimParcel(string trackingNumber)
-    {
-        await parcelService.ClaimParcelAsync(trackingNumber);
-        return NoContent(); // 204
-    }
-
-    [HttpGet("{trackingNumber}")]
-    public async Task<IActionResult> GetParcelByTrackingNumber(string trackingNumber)
-    {
-        var resultParcel = await parcelService.GetParcelByTrackingNumberAsync(trackingNumber);
-        var resultParcelDto = new ParcelResponseDto
+        [HttpPost("{trackingNumber}/claim")]
+        public async Task<IActionResult> ClaimParcel(string trackingNumber)
         {
-            Id = resultParcel!.Id,
-            TrackingNumber = resultParcel!.TrackingNumber,
-            ResidentUnit = resultParcel!.ResidentUnit,
-            Weight = resultParcel!.Weight ?? 0,
-            Dimensions = resultParcel!.Dimensions ?? ""
-        };
-        return Ok(resultParcelDto);
-    }
+            await parcelService.ClaimParcelAsync(trackingNumber);
+            return NoContent(); // 204
+        }
 
-    [HttpGet("awaitingPickup")]
-    public async Task<IActionResult> GetParcelAwaitingPickup()
-    {
-        var parcelsAwaitingPickup = await parcelService.GetParcelsAwaitingPickup();
-        var parcelAwaitingPickupDto = parcelsAwaitingPickup.Select(entity => new ParcelResponseDto
+        [HttpGet("{trackingNumber}")]
+        public async Task<IActionResult> GetParcelByTrackingNumber(string trackingNumber)
         {
-            Id = entity!.Id,
-            TrackingNumber = entity.TrackingNumber,
-            ResidentUnit = entity.ResidentUnit,
-            Weight = entity?.Weight,
-            Dimensions = entity?.Dimensions
-        });
-        return Ok(parcelAwaitingPickupDto);
-    }
+            var resultParcel = await parcelService.GetParcelByTrackingNumberAsync(trackingNumber);
+            var resultParcelDto = new ParcelResponseDto
+            {
+                Id = resultParcel!.Id,
+                TrackingNumber = resultParcel!.TrackingNumber,
+                ResidentUnit = resultParcel!.ResidentUnit,
+                Weight = resultParcel!.Weight ?? 0,
+                Dimensions = resultParcel!.Dimensions ?? ""
+            };
+            return Ok(resultParcelDto);
+        }
 
+        [HttpGet("awaitingPickup")]
+        public async Task<IActionResult> GetParcelAwaitingPickup()
+        {
+            var parcelsAwaitingPickup = await parcelService.GetParcelsAwaitingPickup();
+            var parcelAwaitingPickupDto = parcelsAwaitingPickup.Select(entity => new ParcelResponseDto
+            {
+                Id = entity!.Id,
+                TrackingNumber = entity.TrackingNumber,
+                ResidentUnit = entity.ResidentUnit,
+                Weight = entity?.Weight,
+                Dimensions = entity?.Dimensions
+            });
+            return Ok(parcelAwaitingPickupDto);
+        }
+
+    }
 }

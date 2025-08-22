99275b7a0e1b99d82c34368bc0ee18139a6a724a
Abdul Qawi Kamran <qawitherev@gmail.com>
2025-08-22 13:02:37 +0800

    Completed UserResidentUnitRepoTest

--- src/ParcelManagement.Test/Repository/UserResidentUnitRepoTest.cs
+++ src/ParcelManagement.Test/Repository/UserResidentUnitRepoTest.cs
@@ -79,7 +79,7 @@ namespace ParcelManagement.Test.Repository
         public async Task GetResidentUnitByUser_NotExist_ShouldReturnNull()
         {
             var res = await _uruFixture.UserResidentUnitRepository.GetResidentUnitsByUser(Guid.NewGuid());
-            Assert.Null(res);
+            Assert.Empty(res);
         }
 
         [Fact]
@@ -109,9 +109,24 @@ namespace ParcelManagement.Test.Repository
                     UnitName = "Unit 2"
                 }
             };
+            var urus = new UserResidentUnit[] {
+                new() {
+                    Id = Guid.NewGuid(),
+                    UserId = userId,
+                    ResidentUnitId = residentUnit1Id,
+                    IsActive = true
+                },
+                new() {
+                    Id = Guid.NewGuid(),
+                    UserId = userId,
+                    ResidentUnitId = residentUnit2Id,
+                    IsActive = true
+                }
+             };
+
             await dbContext.Users.AddAsync(user);
             await dbContext.ResidentUnits.AddRangeAsync(residentUnits);
-            
+            await dbContext.UserResidentUnits.AddRangeAsync(urus);
             await dbContext.SaveChangesAsync();
 
             var res = await _uruFixture.UserResidentUnitRepository.GetResidentUnitsByUser(userId);
@@ -132,12 +147,13 @@ namespace ParcelManagement.Test.Repository
         public async Task GetUserByResidentUnit_NotExist_ShouldReturnNull()
         {
             var res = await _uruFixture.UserResidentUnitRepository.GetUsersByResidentUnit(Guid.NewGuid());
-            Assert.Null(res);
+            Assert.Empty(res);
         }
 
         [Fact]
         public async Task GetUserByResidentUnit_Exist_ShouldReturnUsers()
         {
+            var dbContext = _uruFixture.DbContext;
             var user1Id = Guid.NewGuid();
             var user2Id = Guid.NewGuid();
             var users = new User[] {
@@ -147,9 +163,50 @@ namespace ParcelManagement.Test.Repository
                 Email = "",
                 PasswordHash = "",
                 PasswordSalt = ""
-                }
-
+                },
+                new() {
+                Id = user2Id,
+                Username = "",
+                Email = "",
+                PasswordHash = "",
+                PasswordSalt = "" }
             };
+            var residentUnitId = Guid.NewGuid();
+            var residentUnit = new ResidentUnit
+            {
+                Id = residentUnitId,
+                UnitName = "Unit-1"
+            };
+            var urus = new UserResidentUnit[] {
+                new() {
+                    Id = Guid.NewGuid(),
+                    UserId = user1Id,
+                    ResidentUnitId = residentUnitId,
+                    IsActive = true
+                },
+                new() {
+                    Id = Guid.NewGuid(),
+                    UserId = user2Id,
+                    ResidentUnitId = residentUnitId
+
+                }
+            };
+            await dbContext.Users.AddRangeAsync(users);
+            await dbContext.ResidentUnits.AddAsync(residentUnit);
+            await dbContext.UserResidentUnits.AddRangeAsync(urus);
+            await dbContext.SaveChangesAsync();
+
+            var res = await _uruFixture.UserResidentUnitRepository.GetUsersByResidentUnit(residentUnitId);
+            foreach (var r in res)
+            {
+                Assert.NotNull(r);
+            }
+            foreach (var user in users)
+            {
+                Assert.Contains(res, res => res!.Id == user.Id);
+            }
+
+            await _uruFixture.ResetDb();
         }
     }
 }

6490770bdae2cd9a91ea686fef2f50f89c8600eb
Abdul Qawi Bin Kamran <qawitherev@gmail.com>
2025-09-01 00:52:54 +0800

    setup auth module with login and register component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: '', loadChildren: () => import('./auth/auth-module').then(m => m.AuthModule)}
+];

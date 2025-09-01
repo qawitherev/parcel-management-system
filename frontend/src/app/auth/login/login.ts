2821afd2f138af5750401d13dbe0ca0c406eb094
Abdul Qawi Bin Kamran <qawitherev@gmail.com>
2025-09-01 15:33:27 +0800

    Register page done

--- frontend/src/app/auth/login/login.ts
+++ frontend/src/app/auth/login/login.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, ɵInternalFormsSharedModule } from '@angular/forms';
+import { RouterModule } from '@angular/router'; 
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [ɵInternalFormsSharedModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  form: FormGroup
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      emailUsername: [''], 
+      password: ['']
+    })
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log('Login')
+    }
+  }
 }
--- frontend/src/app/auth/register/register.ts
+++ frontend/src/app/auth/register/register.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule, AbstractControl, ValidationErrors } from '@angular/forms';
+import { NgClass } from '@angular/common';
+
+function passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
+  const password = control.get('password')?.value
+  const confirmPassword = control.get('confirmPassword')?.value
+  return password === confirmPassword ? null : 
+    {passwordMismatch: true, errorString: 'Passwords do not match'}
+}
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export class Register {
+  form: FormGroup; 
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', [Validators.required, Validators.minLength(10)]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]], 
+      agreeTerms: [false]
+    }, 
+    // Pass options as the second argument, but use the new object signature:
+    { validators: [passwordMatchValidator] }
+    );
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value)
+    } else {
+      console.info('Register clicked!')
+    }
+  }
 }

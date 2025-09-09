f16766c62480ef2ac17d9866f1c053624d2c88cb
Abdul Qawi Bin Kamran <qawitherev@gmail.com>
2025-09-09 10:56:14 +0800

    Finished check in page and api integration

--- frontend/src/app/features/parcels/check-in/pages/check-in/check-in.ts
+++ frontend/src/app/features/parcels/check-in/pages/check-in/check-in.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { CheckInService } from '../../check-in-service';
+import { Observable, Subject } from 'rxjs';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { AppConsole } from '../../../../../utils/app-console';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-check-in',
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf, AsyncPipe, NgClass],
   templateUrl: './check-in.html',
   styleUrl: './check-in.css'
 })
-export class CheckIn {
+export class CheckIn implements OnDestroy {
+  private destroy$ = new Subject<any>()
+  formGroup: FormGroup
+  checkInResponse$?: Observable<any>
 
+  constructor(private checkInService: CheckInService, private fb: FormBuilder) {
+    this.formGroup = fb.group({
+      trackingNumber: ['', [Validators.required, Validators.maxLength(20)]],
+      residentUnit: ['', [Validators.required, Validators.maxLength(20)]]
+    })
+  }
+
+  onCheckIn() {
+    if (this.formGroup.valid) {
+      AppConsole.log(`Check in starting...`)
+      const payload = {
+        trackingNumber: this.formGroup.value.trackingNumber, 
+        residentUnit: this.formGroup.value.residentUnit
+      }
+      AppConsole.log(`payload: ${JSON.stringify(payload)}`)
+      this.checkInResponse$ = this.checkInService.checkInParcel(payload)
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(null)
+    this.destroy$.complete()
+  }
 }

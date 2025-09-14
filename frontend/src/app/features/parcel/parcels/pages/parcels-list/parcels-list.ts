cd680bd5719aa205c2640f1bcd9065fbbfd948f8
Abdul Qawi Bin Kamran <qawitherev@gmail.com>
2025-09-14 12:58:47 +0800

    working searchbar filter

--- frontend/src/app/features/parcel/parcels/pages/parcels-list/parcels-list.ts
+++ frontend/src/app/features/parcel/parcels/pages/parcels-list/parcels-list.ts
@@ -1,49 +1,90 @@
 import { AfterViewInit, Component, OnInit, ViewChild, viewChild } from '@angular/core';
-import { Observable } from 'rxjs';
+import {
+  BehaviorSubject,
+  debounce,
+  debounceTime,
+  distinctUntilChanged,
+  Observable,
+  Subject,
+  switchMap,
+} from 'rxjs';
 import { ParcelResponseList, ParcelsService } from '../../parcels-service';
 import { AsyncPipe, CommonModule, NgFor } from '@angular/common';
-import { Pagination, PaginationEmitData } from "../../../../../common/components/pagination/pagination";
+import {
+  Pagination,
+  PaginationEmitData,
+} from '../../../../../common/components/pagination/pagination';
 import { AppConsole } from '../../../../../utils/app-console';
+import { FormsModule } from '@angular/forms';
+
+interface ParcelFilter {
+  search: string;
+  status: string;
+}
 
 @Component({
   selector: 'app-parcels-list',
-  imports: [NgFor, AsyncPipe, CommonModule, Pagination],
+  imports: [NgFor, AsyncPipe, CommonModule, Pagination, FormsModule],
   templateUrl: './parcels-list.html',
-  styleUrl: './parcels-list.css'
+  styleUrl: './parcels-list.css',
 })
 export class ParcelsList implements OnInit {
-  parcelList$?: Observable<ParcelResponseList>
-  paginationCurrentPage: number = 1
-  paginationPageSize: number = 1
+  parcelList$ = new BehaviorSubject<ParcelResponseList | null>(null);
+  paginationCurrentPage: number = 1;
+  paginationPageSize: number = 10;
+  filterState: ParcelFilter = {
+    search: '',
+    status: '',
+  };
+  private searchSubject = new Subject<string>();
 
   constructor(private parcelService: ParcelsService) {}
 
   // Reads as:
-  // “In this parent component, create a property called child of type ChildComponent. 
-  // Angular will automatically assign it with the first <app-child> found in the template 
+  // “In this parent component, create a property called child of type ChildComponent.
+  // Angular will automatically assign it with the first <app-child> found in the template
   // after the view is initialized.”
   // @ViewChild(Pagination) paginationChild !: Pagination
 
-    ngOnInit(): void {
-    this.parcelList$ = this.parcelService.getAllParcels(
-          "", 
-          "", 
-          "", 
-          this.paginationCurrentPage, 
-          this.paginationPageSize
+  ngOnInit(): void {
+    this.parcelService
+      .getAllParcels('', '', '', this.paginationCurrentPage, this.paginationPageSize)
+      .subscribe((result) => {
+        this.parcelList$?.next(result);
+      });
+    this.searchSubject
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        switchMap((searchKeyword) =>
+          this.parcelService.getAllParcels(
+            searchKeyword,
+            '',
+            '',
+            this.paginationCurrentPage,
+            this.paginationPageSize
+          )
         )
+      )
+      .subscribe((result) => {
+        this.parcelList$?.next(result)
+      });
   }
 
   onPaginationChanged(data: PaginationEmitData) {
-    AppConsole.log(`PAGINATION: ReceivedPaginationData: ${JSON.stringify(data)}`)
-    this.paginationCurrentPage = data.currentPage
-    this.paginationPageSize = data.pageSize
-    this.parcelList$ = this.parcelService.getAllParcels(
-      "", 
-      "", 
-      "", 
-      this.paginationCurrentPage, 
+    AppConsole.log(`PAGINATION: ReceivedPaginationData: ${JSON.stringify(data)}`);
+    this.paginationCurrentPage = data.currentPage;
+    this.paginationPageSize = data.pageSize;
+    this.parcelService.getAllParcels(
+      '',
+      '',
+      '',
+      this.paginationCurrentPage,
       this.paginationPageSize
-    )
+    ).subscribe((result) => {this.parcelList$.next(result)})
+  }
+
+  onSearch(value: string) {
+    this.searchSubject.next(value);
   }
 }

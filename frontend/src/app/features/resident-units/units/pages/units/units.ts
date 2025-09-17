500a182a71fc5b2eeda4a59f18d38d3ae4841561
Abdul Qawi Bin Kamran <qawitherev@gmail.com>
2025-09-17 17:51:12 +0800

    Added search functionality

--- frontend/src/app/features/resident-units/units/pages/units/units.ts
+++ frontend/src/app/features/resident-units/units/pages/units/units.ts
@@ -1,15 +1,16 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { GetAllResidentUnitsResponse, GetAllUnitsParams, ResidentUnitResponse, UnitsService } from '../../units-service';
-import { BehaviorSubject, map, Observable, Subject, switchMap, take, takeUntil } from 'rxjs';
-import { ApiError } from '../../../../../core/error-handling/api-catch-error';
+import { BehaviorSubject, debounceTime, distinctUntilChanged, map, Observable, Subject, switchMap, take, takeUntil } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { PaginationEmitData, Pagination } from '../../../../../common/components/pagination/pagination';
 import { formatTime } from '../../../../../utils/date-time-utils';
+import { FormsModule } from '@angular/forms';
+import { AppConsole } from '../../../../../utils/app-console';
 
 @Component({
   selector: 'app-units',
   standalone: true,
-  imports: [AsyncPipe, Pagination],
+  imports: [AsyncPipe, Pagination, FormsModule],
   templateUrl: './units.html',
   styleUrl: './units.css',
 })
@@ -20,12 +21,14 @@ export class Units implements OnInit, OnDestroy {
   paginationCurrentPage: number = 1;
   paginationPageSize: number = 10;
 
-  private queryParams = new BehaviorSubject<GetAllUnitsParams>({
+  queryParams = new BehaviorSubject<GetAllUnitsParams>({
     page: this.paginationCurrentPage,
     take: this.paginationPageSize,
   });
 
   unitsList$ = this.queryParams.pipe(
+    debounceTime(300),
+    distinctUntilChanged(),
     switchMap((params) => this.unitsService.getAllUnits(params).pipe(
       map(res => {
         if ('residentUnits' in res) {
@@ -63,5 +66,12 @@ export class Units implements OnInit, OnDestroy {
       page: data.currentPage,
       take: data.pageSize,
     });
+  }
+
+  onSearch(searchKeyword: string) {
+    this.queryParams.next({
+      ...this.queryParams, 
+      unitName: searchKeyword
+    })
   }
 }

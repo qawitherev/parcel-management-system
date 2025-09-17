623dbab07e6a9336dc13de30f1bee5c96aaeab2a
Abdul Qawi Bin Kamran <qawitherev@gmail.com>
2025-09-17 17:23:26 +0800

    Added pagination + integration with api

--- frontend/src/app/features/resident-units/units/pages/units/units.ts
+++ frontend/src/app/features/resident-units/units/pages/units/units.ts
@@ -1,32 +1,50 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { GetAllResidentUnitsResponse, GetAllUnitsParams, UnitsService } from '../../units-service';
-import { Observable, Subject, takeUntil } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, switchMap, take, takeUntil } from 'rxjs';
 import { ApiError } from '../../../../../core/error-handling/api-catch-error';
 import { AsyncPipe } from '@angular/common';
+import { PaginationEmitData, Pagination } from '../../../../../common/components/pagination/pagination';
 
 @Component({
   selector: 'app-units',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, Pagination],
   templateUrl: './units.html',
-  styleUrl: './units.css'
+  styleUrl: './units.css',
 })
 export class Units implements OnInit, OnDestroy {
-  unitsList$?: Observable<GetAllResidentUnitsResponse | ApiError>
-  destroy$ = new Subject<void>()
-  queryParams: GetAllUnitsParams = {}
+  destroy$ = new Subject<void>();
+
+  // pagination state
+  paginationCurrentPage: number = 1;
+  paginationPageSize: number = 10;
+
+  private queryParams = new BehaviorSubject<GetAllUnitsParams>({
+    page: this.paginationCurrentPage,
+    take: this.paginationPageSize,
+  });
+
+  unitsList$ = this.queryParams.pipe(
+    switchMap((params) => this.unitsService.getAllUnits(params)),
+    takeUntil(this.destroy$)
+  );
 
   constructor(private unitsService: UnitsService) {}
 
   ngOnInit(): void {
-    this.unitsList$ = this.unitsService.getAllUnits(this.queryParams)
-      .pipe(
-        takeUntil(this.destroy$)
-      )
+    // do nothing
   }
 
   ngOnDestroy(): void {
-    this.destroy$.next()
-    this.destroy$.complete()
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  onPaginationChanged(data: PaginationEmitData) {
+    this.queryParams.next({
+      ...this.queryParams,
+      page: data.currentPage,
+      take: data.pageSize,
+    });
   }
 }

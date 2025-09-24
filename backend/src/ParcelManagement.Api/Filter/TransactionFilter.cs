2f845e7852ae314ee29574a21ccb407e48736826
Abdul Qawi Bin Kamran <qawitherev@gmail.com>
2025-09-25 00:20:46 +0800

    Finished implementing transactionfilter

--- backend/src/ParcelManagement.Api/Filter/TransactionFilter.cs
+++ backend/src/ParcelManagement.Api/Filter/TransactionFilter.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc.Filters;
 using ParcelManagement.Core.Repositories;
+using ParcelManagement.Core.UnitOfWork;
 using ParcelManagement.Infrastructure.Database;
 
 namespace ParcelManagement.Api.Filter
@@ -7,18 +8,50 @@ namespace ParcelManagement.Api.Filter
     public class TransactionFilter : IAsyncActionFilter
     {
         private readonly ApplicationDbContext _dbContext;
-
-        public TransactionFilter(ApplicationDbContext dbContext)
+        private readonly IUnitOfWork _unitOfWork;
+        public TransactionFilter(
+            ApplicationDbContext dbContext,
+            IUnitOfWork unitOfWork
+            )
         {
             _dbContext = dbContext;
+            _unitOfWork = unitOfWork;
         }
 
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
-            // var method = context.HttpContext.Request.Method;
-            // if (method == HttpMethod.Post.ToString() ||
-            //     method == 
-            // )
+            var method = context.HttpContext.Request.Method;
+            if (method == HttpMethod.Post.ToString() ||
+            method == HttpMethod.Put.ToString() ||
+            method == HttpMethod.Patch.ToString() ||
+            method == HttpMethod.Delete.ToString()
+            )
+            {
+                await using var transaction = await _unitOfWork.BeginTransaction();
+                try
+                {
+                    var result = await next();
+                    if (result.Exception != null)
+                    {
+                        await transaction.RollbackTransactionAsync();
+                    }
+                    else
+                    {
+                        await transaction.CommitTransactionAsync();
+                    }
+
+                }
+                catch
+                {
+                    await transaction.RollbackTransactionAsync();
+                    throw;
+                }
+
+            }
+            else
+            {
+                await next();
+            }
         }
     }
 }

be0d082c096fa1670c6bf9a1e75f339d5f282d74
Abdul Qawi Bin Kamran <qawitherev@gmail.com>
2025-10-08 22:03:36 +0800

    auth service setup unit test

--- frontend/src/app/features/auth/auth.spec.ts
+++ frontend/src/app/features/auth/auth.spec.ts
@@ -1,14 +1,32 @@
 import { TestBed } from '@angular/core/testing';
-
 import { Auth } from './auth';
+import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { RoleService } from '../../core/roles/role-service';
+import { Router } from '@angular/router';
 
 describe('Auth', () => {
   let service: Auth;
+  let httpMock: any;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const roleSpy = jasmine.createSpyObj('RoleService', ['clearRole'])
+    const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl'])
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(), 
+        provideHttpClientTesting(),
+        { provide: RoleService, useValue: roleSpy },
+        { provide: Router, useValue: routerSpy }
+      ]
+    });
     service = TestBed.inject(Auth);
+    httpMock = TestBed.inject(HttpTestingController)
   });
+
+  afterEach(() => {
+    httpMock.verify()
+  })
 
   it('should be created', () => {
     expect(service).toBeTruthy();

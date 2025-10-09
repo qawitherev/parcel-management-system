063c48161a35174a5d8f066f62476d99955bc34d
Abdul Qawi Bin Kamran <qawitherev@gmail.com>
2025-10-09 21:44:33 +0800

    configure karma to have junit reporter to generate test result. also added the test result into gitignore

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -2,6 +2,12 @@ module.exports = function (config) {
   config.set({
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     browsers: ['ChromeHeadlessCI'],
+    reporters: ['progress', 'junit'], 
+    junitReporter: {
+      outputDir: 'test-results', // folder where to save files
+      outputFile: 'unit-test-results-frontend.xml', // name of the report file
+      useBrowserName: false // don't append browser name to the file
+    },
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
@@ -18,9 +24,9 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
+      require('karma-junit-reporter')
     //   require('@angular-devkit/build-angular/plugins/karma')
     ],
-    reporters: ['progress'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,

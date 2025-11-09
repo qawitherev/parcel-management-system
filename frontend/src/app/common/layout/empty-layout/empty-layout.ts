a0919eebae59e2f21341cc09a0a03168e4bf5d06
Abdul Qawi Bin Kamran <qawitherev@gmail.com>
2025-11-09 12:46:49 +0800

    frontend: added reusable switch component

--- frontend/src/app/common/layout/empty-layout/empty-layout.ts
+++ frontend/src/app/common/layout/empty-layout/empty-layout.ts
@@ -3,11 +3,12 @@ import { AuthRoutingModule } from "../../../features/auth/auth-routing-module";
 import { RouterOutlet } from '@angular/router';
 import { ThemeService } from '../../../core/theme/theme-service';
 import { NgClass } from '@angular/common';
+import { MySwitch } from "../../components/switch/my-switch/my-switch";
 
 @Component({
   selector: 'app-empty-layout',
   standalone: true, 
-  imports: [AuthRoutingModule, NgClass],
+  imports: [AuthRoutingModule, NgClass, MySwitch],
   templateUrl: './empty-layout.html',
   styleUrl: './empty-layout.css'
 })

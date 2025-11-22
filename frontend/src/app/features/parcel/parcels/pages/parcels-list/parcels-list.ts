458efa33005ae252f50bd3e4bccb459c960cded1
Abdul Qawi Bin Kamran <qawitherev@gmail.com>
2025-11-22 19:22:59 +0800

    frontend: parcellist enhanced theme

--- frontend/src/app/features/parcel/parcels/pages/parcels-list/parcels-list.ts
+++ frontend/src/app/features/parcel/parcels/pages/parcels-list/parcels-list.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   BehaviorSubject,
   combineLatest,
@@ -7,34 +7,22 @@ import {
   Subject,
   switchMap,
   map,
-  tap
+  tap,
 } from 'rxjs';
 import { ParcelResponse, ParcelResponseList, ParcelsService } from '../../parcels-service';
 import { AsyncPipe, CommonModule, NgFor } from '@angular/common';
-import {
-  Pagination,
-  PaginationEmitData,
-} from '../../../../../common/components/pagination/pagination';
-import { AppConsole } from '../../../../../utils/app-console';
+import { PaginationEmitData } from '../../../../../common/components/pagination/pagination';
 import { FormsModule } from '@angular/forms';
 import { ListingQueryParams } from '../../../../../common/models/listing-query-params';
-import { MyTable, TableColumn } from "../../../../../common/components/table/my-table/my-table";
-
-interface ParcelFilter {
-  search: string;
-  status: string;
-}
-
-
+import { MyTable, TableColumn } from '../../../../../common/components/table/my-table/my-table';
 
 @Component({
   selector: 'app-parcels-list',
-  imports: [NgFor, AsyncPipe, CommonModule, Pagination, FormsModule, MyTable],
+  imports: [NgFor, AsyncPipe, CommonModule, FormsModule, MyTable],
   templateUrl: './parcels-list.html',
   styleUrl: './parcels-list.css',
 })
 export class ParcelsList implements OnInit {
-
   paginationCurrentPage: number = 1;
   paginationPageSize: number = 10;
 
@@ -43,58 +31,54 @@ export class ParcelsList implements OnInit {
     { key: 'locker', label: 'Tracking Number', isActionColumn: false },
     { key: 'weight', label: 'Weight', isActionColumn: false },
     { key: 'dimension', label: 'Dimension', isActionColumn: false },
-    { key: 'residentUnit', label: 'Resident Unit', isActionColumn: false }, 
-    { key: 'status', label: 'Status', isActionColumn: false }
+    { key: 'residentUnit', label: 'Resident Unit', isActionColumn: false },
+    { key: 'status', label: 'Status', isActionColumn: false },
   ];
 
-  searchKeyword = new BehaviorSubject<string>('')
-  statusStream = new BehaviorSubject<string>('All')
+  searchKeyword = new BehaviorSubject<string>('');
+  statusStream = new BehaviorSubject<string>('All');
   paginationParams = new BehaviorSubject<Omit<ListingQueryParams, 'searchKeyword'>>({
-    page: this.paginationCurrentPage, 
-    take: this.paginationPageSize
-  })
+    page: this.paginationCurrentPage,
+    take: this.paginationPageSize,
+  });
 
   parcelList$ = combineLatest([
     this.searchKeyword.pipe(debounceTime(300), distinctUntilChanged()),
     this.statusStream.pipe(distinctUntilChanged()),
-    this.paginationParams.pipe(distinctUntilChanged())
+    this.paginationParams.pipe(distinctUntilChanged()),
   ]).pipe(
     map(([searchKeyword, status, pagination]) => ({
-      searchKeyword, 
+      searchKeyword,
       status,
-      ...pagination
-    })), 
-    switchMap(params => this.parcelService.getAllParcels(params))
-  )
+      ...pagination,
+    })),
+    switchMap((params) => this.parcelService.getAllParcels(params))
+  );
 
-  private searchSubject = new Subject<string>();
-  availableStatus = ["All", "AwaitingPickup", "Claimed"]
-  selectedStatus: string = "All"
-
-  
+  availableStatus = ['All', 'AwaitingPickup', 'Claimed'];
+  selectedStatus: string = 'All';
 
   constructor(private parcelService: ParcelsService) {}
 
-
   ngOnInit(): void {
-    // do nothing 
+    // do nothing
   }
 
   onPaginationChanged(data: PaginationEmitData) {
-    this.paginationCurrentPage = data.currentPage
-    this.paginationPageSize = data.pageSize
+    this.paginationCurrentPage = data.currentPage;
+    this.paginationPageSize = data.pageSize;
     this.paginationParams.next({
-      page: data.currentPage, 
-      take: data.pageSize
-    })
+      page: data.currentPage,
+      take: data.pageSize,
+    });
   }
 
   onSearch(value: string) {
-    this.searchKeyword.next(value)
+    this.searchKeyword.next(value);
   }
 
   onStatusChanged(event: Event) {
-    const status = event.target as HTMLSelectElement
-    this.statusStream.next(status.value.toString())
+    const status = event.target as HTMLSelectElement;
+    this.statusStream.next(status.value.toString());
   }
 }

dcec236f885fd6ea51332368df9e23b2ad484361
Abdul Qawi Bin Kamran <qawitherev@gmail.com>
2025-12-25 16:32:39 +0800

    frontend.service: add notificationPref service

--- frontend/src/app/core/endpoints/notification-prefs-endpoints.ts
+++ frontend/src/app/core/endpoints/notification-prefs-endpoints.ts
@@ -1,5 +1,8 @@
 import { environment } from "../../../environment/environment";
 
 export const notificationPrefEndpoints = {
-    createNotificationPref: `${environment.apiBaseUrl}/v1/notificationPref`
+    createNotificationPref: `${environment.apiBaseUrl}/v1/notificationPref`,
+    getNotificationPref: `${environment.apiBaseUrl}/v1/notificationPref`, 
+    getNotificationPrefByUser: `${environment.apiBaseUrl}/v1/notificationPref/me`, 
+    updateNotificationPref: `${environment.apiBaseUrl}/v1/notificationPref`
 }
--- frontend/src/app/features/system-settings/notification-prefs/notification-prefs-service.ts
+++ frontend/src/app/features/system-settings/notification-prefs/notification-prefs-service.ts
@@ -15,6 +15,16 @@ export interface NotificationPrefCreateRequest {
   quietHoursTo?: string | null;
 }
 
+export interface NotificationPrefUpdateRequest {
+  isEmailActive?: boolean;
+  isWhatsAppActive?: boolean;
+  isOnCheckInActive?: boolean;
+  isOnClaimActive?: boolean;
+  isOverdueActive?: boolean;
+  quietHoursFrom?: string | null;
+  quietHoursTo?: string | null;
+}
+
 export interface NotificationPrefResponse {
   id: string;
   userId: string;
@@ -28,21 +38,47 @@ export interface NotificationPrefResponse {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NotificationPrefsService {
-
   constructor(private http: HttpClient) {}
   /**
-   * create notification-prefs 
-   * get notifiation-prefs 
-   * update notification-prefs 
+   * create notification-prefs
+   * get notifiation-prefs
+   * update notification-prefs
    */
 
-  createNotificationPrefs(requestPayload: NotificationPrefCreateRequest): Observable<NotificationPrefResponse | ApiError> {
+  createNotificationPref(
+    requestPayload: NotificationPrefCreateRequest
+  ): Observable<NotificationPrefResponse | ApiError> {
     const payloadParams: HttpParams = HttpParamsBuilder(requestPayload);
-    return this.http.post<NotificationPrefResponse | ApiError>(notificationPrefEndpoints.createNotificationPref, {payloadParams}).pipe(
-      catchError(handleApiError)
-    )
+    return this.http
+      .post<NotificationPrefResponse | ApiError>(notificationPrefEndpoints.createNotificationPref, {
+        payloadParams,
+      })
+      .pipe(catchError(handleApiError));
+  }
+
+  getNotificationPref(id: string): Observable<NotificationPrefResponse | ApiError> {
+    return this.http.get<NotificationPrefResponse | ApiError>(
+      `${notificationPrefEndpoints.getNotificationPref}/${id}`
+    );
+  }
+
+  getNotificationPrefByUser(): Observable<NotificationPrefResponse | ApiError> {
+    return this.http.get<NotificationPrefResponse | ApiError>(
+      `${notificationPrefEndpoints.getNotificationPrefByUser}`
+    );
+  }
+
+  updateNotificationPref(
+    npId: string,
+    updatePayload: NotificationPrefUpdateRequest
+  ): Observable<NotificationPrefResponse | ApiError> {
+    const body: HttpParams = HttpParamsBuilder(updatePayload);
+    return this.http.patch<NotificationPrefResponse | ApiError>(
+      `${notificationPrefEndpoints.updateNotificationPref}/${npId}`,
+      { body }
+    );
   }
 }

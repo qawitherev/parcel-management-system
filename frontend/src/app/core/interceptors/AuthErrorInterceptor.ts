ece37c1eba8065c8e6f534c9b60051b6f7e09b9e
Abdul Qawi Bin Kamran <qawitherev@gmail.com>
2026-01-22 11:44:37 +0800

    frontend: some fix

--- frontend/src/app/core/interceptors/AuthErrorInterceptor.ts
+++ frontend/src/app/core/interceptors/AuthErrorInterceptor.ts
@@ -1,16 +1,22 @@
 import { HttpErrorResponse, HttpInterceptorFn } from "@angular/common/http";
-import { catchError, throwError } from "rxjs";
+import { catchError, of, throwError } from "rxjs";
 import { AuthService } from "../../features/auth/auth-service";
 import { inject } from "@angular/core";
+import { Router } from "@angular/router";
 
 export const AuthErrorInterceptor: HttpInterceptorFn = (req, next) => {
     const authService: AuthService = inject(AuthService);
+    const router: Router = inject(Router);
     return next(req).pipe(
         catchError((err: HttpErrorResponse) => {
-            if (err.status == 401 && !req.url.includes('refresh') && !req.url.includes('login')) {
+            if (err.status === 401 && !req.url.includes('refresh') && !req.url.includes('login')) {
                 return authService.handleExpiredToken(req, next);
             }
-            return throwError(() => new Error(err.message));
+            if (err.status === 403) {
+                router.navigateByUrl('/systemPages')
+            }
+            // TODO: i think we should remove the handleApiError on individual api call just handle it all here???
+            return throwError(() => err);
         })
     );
 }

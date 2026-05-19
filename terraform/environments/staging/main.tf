880f2cdad88c17d545036a26e5aaff2d28bced2d
Abdul Qawi Bin Kamran <qawitherev@gmail.com>
2026-05-19 13:32:36 +0800

    fix: tighten terraform required_version to >= 1.6 for use_lockfile support

--- terraform/environments/staging/main.tf
+++ terraform/environments/staging/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">=1.0"
+  required_version = ">= 1.6"
   backend "s3" {
     bucket       = "parcel-management-system"
     key          = "terraform/staging/terraform.tfstate"
